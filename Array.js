5c9a9ef74034c47add447c05efe2534df0dfc83a
squidawg <78506807+squidawg@users.noreply.github.com>
2023-02-07 18:12:40 +0400

    init: start unit-test task

---
+++ Array.js
@@ -0,0 +1,5 @@
+const array = {
+    // todo array methods
+}
+
+module.exports = array;
---
+++ Array.test.js
@@ -0,0 +1,5 @@
+const array = require('/Array.js')
+
+test(' description', () => {
+    // expect((array.funcName(args)).toBe(something);
+})
