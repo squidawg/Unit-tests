cc57cf747a6ac1c7e2457c2b9b1a28d28ce61776
squidawg <78506807+squidawg@users.noreply.github.com>
2023-02-08 19:23:47 +0400

    feat: add  chunk, compact, drop, dropWhile

--- Array.js
+++ Array.js
@@ -1,5 +1,57 @@
 const array = {
-    // todo array methods
+    chunk : function (arr, size) {
+    const tempArr = [];
+    if(!size) {
+        return arr
+    }
+    if(!Array.isArray(arr)){
+        return 'not an array';
+    }
+    let i = 0;
+    while (i < arr.length) {
+        tempArr.push(arr.slice(i, i + size));
+        i += size;
+    }
+    return tempArr;
+  },
+    compact: (array) => {
+        const temp = [];
+        let i = 0;
+        if(!Array.isArray(array)) {
+            return 'not an array'
+        }
+        while (i < array.length) {
+            if(array[i]){
+                temp.push(array[i]);
+            }
+            i+=1;
+        }
+        return temp
+
+
+    },
+    drop: (array, n= 1) => {
+        if(!Array.isArray(array)){
+            return 'not an array'
+        }
+        const temp = []
+        while(n < array.length){
+            temp.push(array[n]);
+            n +=1 ;
+        }
+        return temp
+    },
+    dropWhile: (array, callback) => {
+        let i = 0;
+        while(i < array.length) {
+            let result = callback(array[i]);
+            if(!result) {
+                break;
+            }
+            i += 1;
+        }
+        return array.slice(i);
+    },
 }
 
 module.exports = array;
