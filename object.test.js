ca56d3d1dde8aed1248a0f024a0eaef5c73b48cf
squidawg <78506807+squidawg@users.noreply.github.com>
2023-02-16 23:02:06 +0400

    feat: add .omitBy unit test

--- object.test.js
+++ object.test.js
@@ -1,6 +1,7 @@
 const object = require('./object.js');
 const testArr = [1,2,3,4,5];
 const testObj = { 'a': 1, 'b': '2', 'c': 3 };
+
 //merge
 describe('This method is like _.assign except that it recursively merges own and ' +
     'inherited enumerable string keyed properties of source objects into the destination object', () => {
@@ -78,7 +79,7 @@ describe('this method creates an object composed of the own and inherited enumer
     const testObj = { 'a': 1, 'b': '2', 'c': 3 };
     const boolObj = { 'a': true, 'b': '2', 'c': true };
 
-    it('.omitBy should be dedfined', () => {
+    it('.omitBy should be defined', () => {
         expect(object.omitBy).toBeDefined();
     });
 
@@ -90,19 +91,27 @@ describe('this method creates an object composed of the own and inherited enumer
         expect(object.omitBy(x,y)).toEqual(result);
     });
 
-    it('validates callback IsNum tests', () => {
+
+    it.each([
+        [{}],
+        [1],
+        [NaN],
+    ])('Should return { \'a\': 1, \'b\': \'2\', \'c\': 3 } for %O', (x) => {
+        expect(object.omitBy(testObj,x)).toEqual(testObj);
+    });
+
+    it('Validates callback IsNum tests', () => {
         object.omitBy(testObj,mockIsNum);
         expect(mockIsNum.mock.calls).toHaveLength(3);
         expect(mockIsNum.mock.results[1].value).toBeFalsy();
         expect(mockIsNum.mock.lastCall.value = 3);
-
     });
+
     it('validates callback IsString tests', () => {
         object.omitBy(testObj,mockIsString);
         expect(mockIsString.mock.calls).toHaveLength(3);
         expect(mockIsString.mock.results[1].value).toBeTruthy();
         expect(mockIsString.mock.lastCall.value = 3);
-
     });
 });
 
